55c75fb1687018fce6d6afd75ffeae15f4a4004b
Rezwan <t.rewan17@gmail.com>
2023-04-24 22:47:34 +0600

    Created news-dragon project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVYlwt73Mf97n0x_JgcUUMvuR6Uv1gGgk",
+  authDomain: "the-news-dragon-54fa9.firebaseapp.com",
+  projectId: "the-news-dragon-54fa9",
+  storageBucket: "the-news-dragon-54fa9.appspot.com",
+  messagingSenderId: "1014731357404",
+  appId: "1:1014731357404:web:121032f0955a04495bc819"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="">This is home</h2>
+            <h2 className="">This is home</h2>
+            <h2 className="">This is home</h2>
+            <h2 className="">This is home</h2>
+        </div>
+    );
+};
+
+export default Home;
